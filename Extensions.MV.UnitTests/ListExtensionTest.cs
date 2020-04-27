9515b6e88a3197470d4c0c075dc0b0656e83339a
Mathias Dieterich Voelcker <mathias.voelcker@cwi.com.br>
2020-04-27 10:31:49 -0300

    add RemoveEqualElements to ListExtension

--- Extensions.MV.UnitTests/ListExtensionTest.cs
+++ Extensions.MV.UnitTests/ListExtensionTest.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Linq;
 using Xunit;
 
 namespace Extensions.MV.UnitTests
@@ -93,6 +94,85 @@ namespace Extensions.MV.UnitTests
             
             //Assert
             Assert.True(areTheSame);
+        }
+
+        [Fact]
+        public void TestRemoveEqualElements_NoFunc_SameObjects()
+        {
+            //Arrange
+            var el1 = new TestElement(1, "test 1");
+            var el2 = new TestElement(2, "test 2");
+            var el3 = new TestElement(3, "test 3");
+            var el4 = new TestElement(1, "test 1");
+            var list1 = new List<TestElement> { el1, el2 };
+            var list2 = new List<TestElement> { el3, el4 };
+
+            //Act
+            var removedItens = list1.RemoveEqualElements(list2);
+
+            //Assert
+            Assert.Equal(1, list1.Count);
+            Assert.Equal(1, removedItens.Count);
+            Assert.Equal("test 1", removedItens.FirstOrDefault().Name);
+        }
+
+        [Fact]
+        public void TestRemoveEqualElements_NoFunc_DiferentObjects()
+        {
+            //Arrange
+            var el1 = new TestElement(1, "test 1");
+            var el2 = new TestElement(2, "test 2");
+            var el3 = new TestElement(3, "test 3");
+            var el4 = new TestElement(4, "test 1");
+            var list1 = new List<TestElement> { el1, el2 };
+            var list2 = new List<TestElement> { el3, el4 };
+
+            //Act
+            var removedItens = list1.RemoveEqualElements(list2);
+
+            //Assert
+            Assert.Equal(2, list1.Count);
+            Assert.Equal(0, removedItens.Count);
+        }
+
+        [Fact]
+        public void TestRemoveEqualElements_WithFunc_SameId()
+        {
+            //Arrange
+            var el1 = new TestElement(1, "test 1");
+            var el2 = new TestElement(2, "test 2");
+            var el3 = new TestElement(3, "test 3");
+            var el4 = new TestElement(1, "test 4");
+            var list1 = new List<TestElement> { el1, el2 };
+            var list2 = new List<TestElement> { el3, el4 };
+
+            //Act
+            var removedItens = list1.RemoveEqualElements(list2, (x, y) => x.Id == y.Id);
+
+            //Assert
+            Assert.Equal(1, list1.Count);
+            Assert.Equal(1, removedItens.Count);
+            Assert.Equal("test 1", removedItens.FirstOrDefault().Name);
+            Assert.Equal(1, removedItens.FirstOrDefault().Id);
+        }
+
+        [Fact]
+        public void TestRemoveEqualElements_WithFunc_DifferentId()
+        {
+            //Arrange
+            var el1 = new TestElement(1, "test 1");
+            var el2 = new TestElement(2, "test 2");
+            var el3 = new TestElement(3, "test 3");
+            var el4 = new TestElement(4, "test 1");
+            var list1 = new List<TestElement> { el1, el2 };
+            var list2 = new List<TestElement> { el3, el4 };
+
+            //Act
+            var removedItens = list1.RemoveEqualElements(list2, (x, y) => x.Id == y.Id);
+
+            //Assert
+            Assert.Equal(2, list1.Count);
+            Assert.Equal(0, removedItens.Count);
         }
     }
 
--- Extensions.MV/ListExtension.cs
+++ Extensions.MV/ListExtension.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Extensions.MV
 {
@@ -29,6 +30,8 @@ namespace Extensions.MV
         ///Checks whether two lists have at least one element in common.
         ///<para/>
         ///The <code>areEqualFunc</code> parameter is a function used to compare both elements.
+        ///<para/>
+        ///Example of usage: <code>list1.AnyEqualElement(list2, (x, y) => x.Id == y.Id)</code>
         ///</summary>
         public static bool AnyEqualElement<T>(this List<T> thisList, List<T> listToCompare, Func<T,T, bool> areEqualFunc)
         {
@@ -40,6 +43,60 @@ namespace Extensions.MV
                 }
             }
             return false;
+        }
+
+        ///<summary>
+        ///Remove every element of the list that exists in a given list.
+        ///<para/>
+        ///The <code>areEqualFunc</code> parameter is a function used to compare both elements.
+        ///<para/>
+        ///Example of usage: <code>list1.RemoveEqualElements(list2, (x, y) => x.Id == y.Id)</code>
+        ///</summary>
+        public static List<T> RemoveEqualElements<T>(this List<T> thisList, List<T> listToCompare, Func<T,T, bool> areEqualFunc)
+        {
+            var removedElements = new List<T>();
+            var i = 0;
+            while(i < thisList.Count)
+            {
+                var elementA = thisList[i];
+                if (listToCompare.Any(x => areEqualFunc(elementA, x)))
+                {
+                    thisList.RemoveAt(i);
+                    removedElements.Add(elementA);
+                }
+                else 
+                {
+                    i++;
+                }
+            }
+
+            return removedElements;
+        }
+
+        ///<summary>
+        ///Remove every element of the list that exists in a given list.
+        ///<para/>
+        ///The method Object.Equals is used to compare both elements.
+        ///</summary>
+        public static List<T> RemoveEqualElements<T>(this List<T> thisList, List<T> listToCompare)
+        {
+            var removedElements = new List<T>();
+            var i = 0;
+            while(i < thisList.Count)
+            {
+                var elementA = thisList[i];
+                if (listToCompare.Any(x => elementA.Equals(x)))
+                {
+                    thisList.RemoveAt(i);
+                    removedElements.Add(elementA);
+                }
+                else 
+                {
+                    i++;
+                }
+            }
+
+            return removedElements;
         }
     }
 }
